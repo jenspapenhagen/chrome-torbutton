4f0416ea8a702cbff0f34e1e05fea357ddbd6fe7
jenspapenhagen <jens.papenhagen@web.de>
2016-08-29 12:16:56 +0200

    snipple coll.

--- content_script.js
+++ content_script.js
@@ -1,22 +1,29 @@
-double randomWithRange(double min, double max)
-{
-   double range = Math.abs(max - min);     
-   return (Math.random() * range) + (min <= max ? min : max);
+double randomWithRange(double min, double max) {
+   double range = Math.abs(max - min); 
+ 
+return (Math.random() * range) + (min <= max ? min : max);
 }
 
-//get random line form cloudflear domain list 
+//get random line form cloudflare domain list 
 //source crimeflare.com [23 August 2016]
-$.get('cfdomains/cflist'+randomWithRange(1, 24)+'.txt', function(txt) {
-    var lines = txt.responseText.split("\n");
-    var randLineNum = Math.floor(Math.random() * lines.length);
-    save(lines[randLineNum]); // random line from the text file
-});
+$.get('cfdomains/cflist'+randomWithRange(1, 24)+'.txt', function(data) {
+     var domain = data.split("\@");
+     var idx = Math.floor(domain.length * Math.random());
+     var line = domain[idx];
+	 var line[1];//get only the domain
+ });
 
 
+//lines[randLineNum].split(" ")
+//besauce the cflist.txt files are build up like this.
+// 2016-08-17: 0--000.com 103.7.43.26
+//and we only need the 2 part.
 
 //tabs opener
-chrome.browserAction.onClicked.addListener(function(activeTab)
-{
-    var newURL = "http://www.youtube.com/watch?v=oHg5SJYRHA0";
-    chrome.tabs.create({ url: newURL });
-});
+//use this >2 times to generate a realistic looking cloudflare cookie (  __cfduid cookie )
+for(i=0; i > randomWithRange(2, 10); i++){
+	chrome.browserAction.onClicked.addListener(function(activeTab) {
+		var newURL = line[1];
+		chrome.tabs.create({ url: newURL });
+	});
+}
